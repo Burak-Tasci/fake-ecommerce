ae34b9ebcdc1878e437ed2c3f29b37f3465a47e3
buraaktascii <buraaktascii@outlook.com>
2022-09-08 23:03:04 +0300

    update | migration on project level build.gradle

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "7.4.0-alpha10" apply false
+    id("com.android.library") version "7.4.0-alpha10" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("com.google.dagger.hilt.android") version "2.41" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.5.1" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("org.jetbrains.kotlin.jvm") version "1.7.10" apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
+    repositories {
+        // Required to download KtLint
+        mavenCentral()
+    }
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
