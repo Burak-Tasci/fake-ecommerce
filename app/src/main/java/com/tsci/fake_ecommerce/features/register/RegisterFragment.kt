67951fd5c5cffc72147e7a1a75617c5339b8992d
buraaktascii <buraaktascii@outlook.com>
2022-09-09 21:53:00 +0300

    update | register fragment layout & add | listener for to login screen text

--- app/src/main/java/com/tsci/fake_ecommerce/features/register/RegisterFragment.kt
+++ app/src/main/java/com/tsci/fake_ecommerce/features/register/RegisterFragment.kt
@@ -1,11 +1,14 @@
 package com.tsci.fake_ecommerce.features.register
 
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.dogancan.core.base.platform.BaseFragment
 import com.dogancan.core.base.platform.BaseViewModel
 import com.dogancan.core.utils.binding.viewBinding
 import com.tsci.fake_ecommerce.R
 import com.tsci.fake_ecommerce.databinding.FragmentRegisterBinding
+import com.tsci.ui.extension.makeLinks
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,7 +18,15 @@ class RegisterFragment : BaseFragment() {
     private val binding: FragmentRegisterBinding by viewBinding()
 
     override fun initView() {
-
+        binding.tvToLogin.makeLinks(
+            Pair(
+                getString(R.string.to_login_highlighted_text), View.OnClickListener{
+                    findNavController().navigate(
+                        RegisterFragmentDirections.toLoginFragment()
+                    )
+                }
+            )
+        )
     }
 
     override fun layoutRes(): Int = R.layout.fragment_register
