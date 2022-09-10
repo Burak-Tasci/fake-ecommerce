dd5b9d7e79e9a0a47e0e2c5b11d9e481a688f92c
buraaktascii <buraaktascii@outlook.com>
2022-09-11 02:00:13 +0300

    create | auth service

--- data/src/main/java/com/tsci/data/remote/AuthService.kt
+++ data/src/main/java/com/tsci/data/remote/AuthService.kt
@@ -1,7 +1,15 @@
 package com.tsci.data.remote
 
+import com.skydoves.sandwich.ApiResponse
+import com.tsci.entity.auth.RegisterRequestModel
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 /**
  * Created by Burak Taşcı on 10.09.2022.
  */
 interface AuthService {
+
+    @POST("users")
+    suspend fun register(@Body requestModel: RegisterRequestModel): ApiResponse<RegisterRequestModel>
 }
