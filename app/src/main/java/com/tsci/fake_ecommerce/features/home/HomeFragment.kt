618d8392f78e56f3721424664a4a88d5ba34d5d1
buraaktascii <buraaktascii@outlook.com>
2022-09-29 17:40:58 +0300

    add | products presentation on home screen
    Signed-off-by: buraaktascii <buraaktascii@outlook.com>

--- app/src/main/java/com/tsci/fake_ecommerce/features/home/HomeFragment.kt
+++ app/src/main/java/com/tsci/fake_ecommerce/features/home/HomeFragment.kt
@@ -6,6 +6,9 @@ import com.dogancan.core.base.platform.BaseViewModel
 import com.dogancan.core.utils.binding.viewBinding
 import com.tsci.fake_ecommerce.R
 import com.tsci.fake_ecommerce.databinding.FragmentHomeBinding
+import com.tsci.fake_ecommerce.extensions.collects
+import com.tsci.fake_ecommerce.extensions.toast
+import com.tsci.fake_ecommerce.features.home.adapter.ProductsAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,7 +17,20 @@ class HomeFragment : BaseFragment() {
     private val viewModel: HomeViewModel by viewModels()
     private val binding: FragmentHomeBinding by viewBinding()
 
+    private val mProductsAdapter = ProductsAdapter()
+
     override fun initView() {
+        binding.rvProducts.adapter = mProductsAdapter
+        viewModel.uiState.collects(viewLifecycleOwner) { result ->
+            when (result){
+                is HomeViewModel.UiState.Success -> {
+                    mProductsAdapter.submitList(result.data)
+                }
+                is HomeViewModel.UiState.Error -> {
+                    toast(result.error.localizedMessage)
+                }
+            }
+        }
 
     }
 
--- app/src/main/java/com/tsci/fake_ecommerce/features/home/HomeViewModel.kt
+++ app/src/main/java/com/tsci/fake_ecommerce/features/home/HomeViewModel.kt
@@ -1,11 +1,50 @@
 package com.tsci.fake_ecommerce.features.home
 
+import androidx.lifecycle.viewModelScope
 import com.dogancan.core.base.platform.BaseViewModel
+import com.tsci.ui.model.product.ProductUiModel
+import com.tsci.usecase.product.IGetAllProductsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : BaseViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getAllProductsUseCase: IGetAllProductsUseCase
+) : BaseViewModel() {
 
+    private val _uiState = MutableStateFlow<UiState>(UiState.Empty)
+    val uiState: StateFlow<UiState> = _uiState
 
+    init {
+        getAllProducts()
+    }
+
+    fun getAllProducts() {
+        viewModelScope.launch {
+            invokeUseCase(
+                getAllProductsUseCase.getAllProducts(),
+                onSuccess = { productList ->
+                    _uiState.update {
+                        UiState.Success(productList)
+                    }
+                },
+                onError = { throwable ->
+                    _uiState.update {
+                        UiState.Error(throwable)
+                    }
+                }
+            )
+        }
+    }
+
+    sealed interface UiState {
+        data class Success(val data: List<ProductUiModel>) : UiState
+        object Loading : UiState
+        data class Error(val error: Throwable) : UiState
+        object Empty : UiState
+    }
 }
