c3d760d8afc04c4e4367cb2f33592dd7999351d5
buraaktascii <buraaktascii@outlook.com>
2022-09-29 17:39:47 +0300

    add | implementation of get all products request
    Signed-off-by: buraaktascii <buraaktascii@outlook.com>

--- data/src/main/java/com/tsci/data/repository/ProductRepository.kt
+++ data/src/main/java/com/tsci/data/repository/ProductRepository.kt
@@ -1,11 +1,28 @@
 package com.tsci.data.repository
 
 import com.dogancan.core.base.network.BaseRepository
+import com.skydoves.sandwich.onFailure
+import com.skydoves.sandwich.onSuccess
+import com.tsci.data.di.IoDispatcher
+import com.tsci.data.remote.ProductService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 /**
  * Created by Burak Taşcı on 10.09.2022.
  */
-class ProductRepository @Inject constructor(): BaseRepository() {
+class ProductRepository @Inject constructor(
+    private val productService: ProductService,
+    @IoDispatcher private val ioScope: CoroutineDispatcher
+): BaseRepository() {
 
+    suspend fun getAllProducts() = invoke {
+        val data = productService.getAllProducts()
+        data.onSuccess {
+            Result.success(this)
+        }.onFailure {
+            Result.failure<Exception>(Exception(this))
+        }
+    }.flowOn(ioScope)
 }
