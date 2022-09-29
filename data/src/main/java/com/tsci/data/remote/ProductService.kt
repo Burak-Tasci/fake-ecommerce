a72dfb76832a6643deb3c2f8015ac1014341435d
buraaktascii <buraaktascii@outlook.com>
2022-09-29 17:39:33 +0300

    add | get all products request
    Signed-off-by: buraaktascii <buraaktascii@outlook.com>

--- data/src/main/java/com/tsci/data/remote/ProductService.kt
+++ data/src/main/java/com/tsci/data/remote/ProductService.kt
@@ -1,7 +1,19 @@
 package com.tsci.data.remote
 
+import com.skydoves.sandwich.ApiResponse
+import com.tsci.entity.product.ProductEntityModel
+import retrofit2.http.GET
+
 /**
  * Created by Burak Taşcı on 10.09.2022.
  */
 interface ProductService {
+
+    @GET(PRODUCTS)
+    suspend fun getAllProducts(): ApiResponse<List<ProductEntityModel>>
+
+
+    companion object{
+        private const val PRODUCTS = "products"
+    }
 }
